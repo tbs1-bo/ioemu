dfa45e072aff2e129ef2e07ecca12de0e28f8365
Marco Bakera <marco@bakera.de>
2018-11-10 12:22:18 +0100

    create README from notebook. added doc.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
+# create distribution files
 python3 setup.py sdist bdist_wheel
+
+# upload files to pypi
 twine upload dist/*
+
+# create documentation from notebook
+jupyter nbconvert README.ipynb --to markdown
