0396e7c489338aadeae709b9764675c625e80974
Marco Bakera <marco@bakera.de>
2018-11-11 13:36:16 +0100

    doc creted before distribution is created.

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
+# remove old files
+rm -rf dist/*
+
+# create documentation from notebook
+jupyter nbconvert README.ipynb --to markdown
+
 # create distribution files
 python3 setup.py sdist bdist_wheel
 
 # upload files to pypi
 twine upload dist/*
-
-# create documentation from notebook
-jupyter nbconvert README.ipynb --to markdown
