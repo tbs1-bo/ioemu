40c5252bf94cb973b8d55f31545303a311daf424
Marco Bakera <marco@bakera.de>
2018-11-21 10:15:12 +0100

    demo client port can be configured.

--- ioemu/demo.py
+++ ioemu/demo.py
@@ -1,8 +1,14 @@
+import os
 import time
 
 import ioemu
 
-emu = ioemu.Emulator()
+# Port to which the demo client talks
+# Can be overriden by an environmental variable.
+DEMO_CLIENT_PORT = int(os.environ.get('IOEMU_DEMO_CLIENT_PORT', default=ioemu.TCP_SERVER_PORT))
+
+
+emu = ioemu.Emulator(port=DEMO_CLIENT_PORT)
 last_btn_state = emu.buttons
 last_analog_val = emu.analog_value
 last_update = time.time()
