d590258257bc0bb42d7142bf250d6b146be75291
Marco Bakera <marco@bakera.de>
2024-11-30 10:25:37 +0100

    pypi use token

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 # create documentation from notebook
 jupyter nbconvert README.ipynb --to markdown
 
+source pypi_credentials.sh
+echo "Using username: $PYPI_USERNAME"
+
 poetry build
-poetry publish
+poetry publish --username $PYPI_USERNAME --password $PYPI_PASSWORD
 
